827e4dfdab53a874ad5bbb929a5a54bf3f2521d7
Feliipe <FelipeGH9304@outlook.com>
2021-09-16 18:09:12 -0500

    hot fix

--- src/ui/hooks/useRequestListData.ts
+++ src/ui/hooks/useRequestListData.ts
@@ -1,23 +1,25 @@
 import {useEffect, useState} from "react";
 import {useIsMounted} from "./useIsMounted";
 import {RequestTokenFunction, ResultData} from "../interfaces/RequestFunction";
-//import {useSelector} from "react-redux";
+import {useSelector} from "react-redux";
+import {RootState} from "../../store/mainStore";
 
 export const useRequestListData = (requestFunc: RequestTokenFunction) => {
     const [loading, setLoading] = useState(false);
     const [listData, setListData] = useState<Array<any>>([]);
     const isMounted = useIsMounted();
-    //const {token} = useSelector((state) => state.signInReducer);
-    const token = "";
+    const {token} = useSelector((state: RootState) => state.loginReducer);
 
     const init = () => {
         if (isMounted.current) setLoading(true);
-        requestFunc(token).then((result: ResultData) => {
-            if (result.resolved && isMounted.current) {
-                setListData([...result.list]);
-            }
-            if (isMounted.current) setLoading(false);
-        });
+        if (token) {
+            requestFunc(token).then((result: ResultData) => {
+                if (result.resolved && isMounted.current) {
+                    setListData([...result.list]);
+                }
+                if (isMounted.current) setLoading(false);
+            });
+        }
     };
 
     useEffect(init, []);
